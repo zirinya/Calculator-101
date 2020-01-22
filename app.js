6e8f59f7a386b2e795ddc0d6671b831335997f8f
Nitipon Numlamun <38374787+Astroly@users.noreply.github.com>
2020-01-22 16:25:58 +0700

    test

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
-
-
 function inp(value){
-  
-//  var x = document.getElementById(showNum).num;
- var num = document.getElementById("showNum").innerHTML += value;
+  var num = document.getElementById("showNum").innerHTML += value;
 
 }
-
-
-function slo(){
+function cal(){
   let num1 = document.getElementById("showNum").innerHTML
   let num2 = eval(num1)
   document.getElementById("showNum").innerHTML = num2
-}
-
-// function oparation(optvalue){
-//   var opt = document.getElementById("showNum").innerHTML = optvalue;
-//   console.log(opt);
-  
-// }
-function clr() 
+ }
+ function clr() 
 { 
-    document.getElementById("showNum").innerHTML = ""
-   
+   document.getElementById("showNum").innerHTML = ""
+  
 } 
