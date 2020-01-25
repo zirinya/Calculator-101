d80da23b59818d8081de3af70e42c4dac5fb80ed
zi <sirinya.hwanphet@gmail.com>
2020-01-25 23:36:56 +0700

    /

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
-
-
-function inp(value){
-  
-//  var x = document.getElementById(showNum).num;
- var num = document.getElementById("showNum").innerHTML += value;
-
+function inp(value) {
+  let num = (document.getElementById("showNum").innerHTML += value);
+}
+function slo() {
+  let num1 = document.getElementById("showNum").innerHTML;
+  let num2 = eval(num1);
+  document.getElementById("showNum").innerHTML = num2;
+}
+function clr() {
+  document.getElementById("showNum").innerHTML = "";
 }
 
-
-function slo(){
-  let num1 = document.getElementById("showNum").innerHTML
-  let num2 = eval(num1)
-  document.getElementById("showNum").innerHTML = num2
-}
-
-// function oparation(optvalue){
-//   var opt = document.getElementById("showNum").innerHTML = optvalue;
-//   console.log(opt);
-  
-// }
-function clr() 
-{ 
-    document.getElementById("showNum").innerHTML = ""
-   
-} 
+//naming 
+//start number
